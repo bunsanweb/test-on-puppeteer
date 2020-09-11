8090d2f03d6f1be09504770c9aaa7727d4e8d0d4
bellbind <bellbind@gmail.com>
2020-09-11 17:39:03 +0900

    [update] playwright-1.4.0
    - chromium-86, firefox-80, webkit-14

--- test/es-proposals/es2021.js
+++ test/es-proposals/es2021.js
@@ -5,11 +5,41 @@ describe("ES2021", function () {
     chai.assert.equal("a-b-c".replaceAll(/-/g, "+"), "a+b+c");
     chai.expect(_ => "a-b-c".replaceAll(/-/, "+")).to.throw();
   });
-  it("numeric eparator _", function () {
+  it("Promise.any()", function (done) {
+    (async () => {
+      const v = await Promise.any([
+        Promise.reject(0),
+        new Promise(f => setTimeout(f, 10, 1)),
+      ]);
+      chai.assert.equal(v, 1);      
+    })().then(done, done);
+  });
+  it("numeric sparator _", function () {
     chai.assert.equal(1_2_3, 123);
     chai.assert.equal(0b10_11_00, 0b101100);
     chai.assert.equal(0xff_cd, 0xffcd);
   });
+  it("a ||= b, a &&= b, a ??= b", function () {
+    let a1 = 0, a2 = 1;
+    a1 ||= 10, a2 ||= 10;
+    chai.assert.equal(a1, 10);
+    chai.assert.equal(a2, 1);
+    
+    let b1 = 0, b2 = 1;
+    b1 &&= 10, b2 &&= 10;
+    chai.assert.equal(b1, 0);
+    chai.assert.equal(b2, 10);
+
+    let c1 = 0, c2 = null;
+    b1 ??= 10, c2 ??= 10;
+    chai.assert.equal(c1, 0);
+    chai.assert.equal(c2, 10);
+  });
+  it.skip("WeakRef", function () {
+    // WeakRef is not implemented in WebKit-14
+    chai.assert.isOk(WeakRef);
+  });
+  
   it("field declarations in class", function () {
     const c = class {
       f = 1;
